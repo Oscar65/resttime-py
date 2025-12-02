11141e604e7086cebece46f84f155c63876e446c
Oscar Megía López <megia.oscar@gmail.com>
2025-12-02 07:35:52 +0100

    Formatted style by pycodestyle

--- resttime.py
+++ resttime.py
@@ -9,9 +9,9 @@ if sys.platform == "linux":
 
 try:
     import mpv
-    _loadedMPV=True
+    _loadedMPV = True
 except ImportError as e:
-    _loadedMPV=False
+    _loadedMPV = False
     print(f"No se pudo cargar la librería mpv: {e}")
 
 if sys.platform == "win32":
@@ -20,7 +20,9 @@ if sys.platform == "win32":
 
 from datetime import datetime
 
+
 class RestTime:
+
     def __init__(self):
         self.timer_activo = False
         self.timer_contador = 0
@@ -48,7 +50,8 @@ class RestTime:
                 prop = root.get_full_property(atom, X.AnyPropertyType)
                 self.window_id = prop.value[0]
                 if prop and prop.value:
-                    self.log(f"Encontrado ID de Ventana: {hex(self.window_id)}")
+                    self.log(
+                        f"Encontrado ID de Ventana: {hex(self.window_id)}")
             except Exception as err:
                 self.log(err)
         elif sys.platform == "win32":
@@ -60,84 +63,126 @@ class RestTime:
         self._mainframe.grid(row=0, column=0, sticky=(tk.E, tk.W, tk.N, tk.S))
 
         self._tiempo_frame = ttk.Frame(self._mainframe, padding='0 0 0 0')
-        self._tiempo_frame.grid(row=0, column=0, sticky=(tk.E, tk.W, tk.N, tk.S))
+        self._tiempo_frame.grid(row=0, column=0, sticky=(tk.E, tk.W, tk.N,
+                                                         tk.S))
 
         self._horas1_sv = tk.StringVar()
         self._horas1_sv.set(0)
-        self._horas1 = ttk.Entry(self._tiempo_frame, textvariable=self._horas1_sv, width=1, font=('Arial', 150))
+        self._horas1 = ttk.Entry(self._tiempo_frame,
+                                 textvariable=self._horas1_sv, width=1,
+                                 font=('Arial', 150))
         self._horas1.grid(row=0, column=1, sticky=(tk.S, tk.N))
         vcmdh1 = (self._root.register(self.validar_entrada_0a9), '%P')
         self._horas1.config(validate="key", validatecommand=vcmdh1)
-        self._scale_horas1 = ttk.Scale(self._tiempo_frame, orient=tk.VERTICAL, command=self._horas1_update, from_=9, to=0)
+        self._scale_horas1 = ttk.Scale(self._tiempo_frame, orient=tk.VERTICAL,
+                                       command=self._horas1_update, from_=9,
+                                       to=0)
         self._scale_horas1.grid(row=0, column=2, sticky=(tk.S, tk.N))
 
         self._horas2_sv = tk.StringVar()
         self._horas2_sv.set(0)
-        self._horas2 = ttk.Entry(self._tiempo_frame, textvariable=self._horas2_sv, width=1, font=('Arial', 150))
+        self._horas2 = ttk.Entry(self._tiempo_frame,
+                                 textvariable=self._horas2_sv, width=1,
+                                 font=('Arial', 150))
         self._horas2.grid(row=0, column=3, sticky=(tk.S, tk.N))
         vcmdh2 = (self._root.register(self.validar_entrada_0a9), '%P')
         self._horas2.config(validate="key", validatecommand=vcmdh2)
-        self._scale_horas2 = ttk.Scale(self._tiempo_frame, orient=tk.VERTICAL, command=self._horas2_update, from_=9, to=0)
+        self._scale_horas2 = ttk.Scale(self._tiempo_frame, orient=tk.VERTICAL,
+                                       command=self._horas2_update,
+                                       from_=9, to=0)
         self._scale_horas2.grid(row=0, column=4, sticky=(tk.S, tk.N))
 
-        self._colon_horas = ttk.Label(self._tiempo_frame, width=0, font=('Arial', 150), text=":")
+        self._colon_horas = ttk.Label(self._tiempo_frame, width=0,
+                                      font=('Arial', 150), text=":")
         self._colon_horas.grid(row=0, column=5, sticky=(tk.S, tk.N))
 
         self._minutos1_sv = tk.StringVar()
         self._minutos1_sv.set(0)
-        self._minutos1 = ttk.Entry(self._tiempo_frame, textvariable=self._minutos1_sv, width=1, font=('Arial', 150))
+        self._minutos1 = ttk.Entry(self._tiempo_frame,
+                                   textvariable=self._minutos1_sv, width=1,
+                                   font=('Arial', 150))
         self._minutos1.grid(row=0, column=6, sticky=(tk.S, tk.N))
         vcmdm1 = (self._root.register(self.validar_entrada_0a5), '%P')
         self._minutos1.config(validate="key", validatecommand=vcmdm1)
-        self._scale_minutos1 = ttk.Scale(self._tiempo_frame, orient=tk.VERTICAL, command=self._minutos1_update, from_=5, to=0)
+        self._scale_minutos1 = ttk.Scale(self._tiempo_frame,
+                                         orient=tk.VERTICAL,
+                                         command=self._minutos1_update,
+                                         from_=5, to=0)
         self._scale_minutos1.grid(row=0, column=7, sticky=(tk.S, tk.N))
 
         self._minutos2_sv = tk.StringVar()
         self._minutos2_sv.set(0)
-        self._minutos2 = ttk.Entry(self._tiempo_frame, textvariable=self._minutos2_sv, width=1, font=('Arial', 150))
+        self._minutos2 = ttk.Entry(self._tiempo_frame,
+                                   textvariable=self._minutos2_sv, width=1,
+                                   font=('Arial', 150))
         self._minutos2.grid(row=0, column=8, sticky=(tk.S, tk.N))
         vcmdm2 = (self._root.register(self.validar_entrada_0a9), '%P')
         self._minutos2.config(validate="key", validatecommand=vcmdm2)
-        self._scale_minutos2 = ttk.Scale(self._tiempo_frame, orient=tk.VERTICAL, command=self._minutos2_update, from_=9, to=0)
+        self._scale_minutos2 = ttk.Scale(self._tiempo_frame,
+                                         orient=tk.VERTICAL,
+                                         command=self._minutos2_update,
+                                         from_=9, to=0)
         self._scale_minutos2.grid(row=0, column=9, sticky=(tk.S, tk.N))
 
-        self._colon_minutos = ttk.Label(self._tiempo_frame, width=0, font=('Arial', 150), text=":")
+        self._colon_minutos = ttk.Label(self._tiempo_frame, width=0,
+                                        font=('Arial', 150), text=":")
         self._colon_minutos.grid(row=0, column=10, sticky=(tk.S, tk.N))
 
         self._segundos1_sv = tk.StringVar()
         self._segundos1_sv.set(0)
-        self._segundos1 = ttk.Entry(self._tiempo_frame, textvariable=self._segundos1_sv, width=1, font=('Arial', 150))
+        self._segundos1 = ttk.Entry(self._tiempo_frame,
+                                    textvariable=self._segundos1_sv,
+                                    width=1, font=('Arial', 150))
         self._segundos1.grid(row=0, column=11, sticky=(tk.S, tk.N))
         vcmds1 = (self._root.register(self.validar_entrada_0a5), '%P')
         self._segundos1.config(validate="key", validatecommand=vcmds1)
-        self._scale_segundos1 = ttk.Scale(self._tiempo_frame, orient=tk.VERTICAL, command=self._segundos1_update, from_=5, to=0)
+        self._scale_segundos1 = ttk.Scale(self._tiempo_frame,
+                                          orient=tk.VERTICAL,
+                                          command=self._segundos1_update,
+                                          from_=5, to=0)
         self._scale_segundos1.grid(row=0, column=12, sticky=(tk.S, tk.N))
 
         self._segundos2_sv = tk.StringVar()
         self._segundos2_sv.set(0)
-        self._segundos2 = ttk.Entry(self._tiempo_frame, textvariable=self._segundos2_sv, width=1, font=('Arial', 150))
+        self._segundos2 = ttk.Entry(self._tiempo_frame,
+                                    textvariable=self._segundos2_sv, width=1,
+                                    font=('Arial', 150))
         self._segundos2.grid(row=0, column=13, sticky=(tk.S, tk.N))
         vcmds2 = (self._root.register(self.validar_entrada_0a9), '%P')
         self._segundos2.config(validate="key", validatecommand=vcmds2)
-        self._scale_segundos2 = ttk.Scale(self._tiempo_frame, orient=tk.VERTICAL, command=self._segundos2_update, from_=9, to=0)
+        self._scale_segundos2 = ttk.Scale(self._tiempo_frame,
+                                          orient=tk.VERTICAL,
+                                          command=self._segundos2_update,
+                                          from_=9, to=0)
         self._scale_segundos2.grid(row=0, column=14, sticky=(tk.S, tk.N))
 
-        self._start_btn = ttk.Button(self._tiempo_frame, text="Start", underline=0, command=self._start_timer, padding="5 5 5 5")
+        self._start_btn = ttk.Button(self._tiempo_frame, text="Start",
+                                     underline=0, command=self._start_timer,
+                                     padding="5 5 5 5")
         self._root.bind('<Alt-i>', lambda event: self._start_timer())
-        self._start_btn.grid(row=1, column=13, sticky=(tk.E, tk.W), padx=10, pady=10)
+        self._start_btn.grid(row=1, column=13, sticky=(tk.E, tk.W), padx=10,
+                             pady=10)
         self._start_btn.rowconfigure(1, weight=1)
 
-        self._stop_btn = ttk.Button(self._tiempo_frame, text="Stop", underline=0, command=self._stop_timer, padding="5 5 5 5")
+        self._stop_btn = ttk.Button(self._tiempo_frame, text="Stop",
+                                    underline=0, command=self._stop_timer,
+                                    padding="5 5 5 5")
         self._root.bind('<Alt-i>', lambda event: self._stop_timer())
-        self._stop_btn.grid(row=1, column=11, sticky=(tk.E, tk.W), padx=10, pady=10)
+        self._stop_btn.grid(row=1, column=11, sticky=(tk.E, tk.W), padx=10,
+                            pady=10)
         self._stop_btn.rowconfigure(1, weight=1)
 
-        self._log_frame = ttk.LabelFrame(self._mainframe, text="Log", padding="10")
-        self._log_frame.grid(row=2, column=0, pady=10, sticky=(tk.E, tk.W, tk.N, tk.S))
+        self._log_frame = ttk.LabelFrame(self._mainframe, text="Log",
+                                         padding="10")
+        self._log_frame.grid(row=2, column=0, pady=10, sticky=(tk.E, tk.W,
+                                                               tk.N, tk.S))
 
-        self._text_log = tk.Text(self._log_frame, height=6, font=('Courier', 10))
-        self._text_log.grid(row=0, column=0, pady=5, sticky=(tk.E, tk.W, tk.N, tk.S))
-        self._scrollbar_tl = ttk.Scrollbar(self._log_frame, orient=tk.VERTICAL, command=self._text_log.yview)
+        self._text_log = tk.Text(self._log_frame, height=6,
+                                 font=('Courier', 10))
+        self._text_log.grid(row=0, column=0, pady=5, sticky=(tk.E, tk.W, tk.N,
+                                                             tk.S))
+        self._scrollbar_tl = ttk.Scrollbar(self._log_frame, orient=tk.VERTICAL,
+                                           command=self._text_log.yview)
         self._scrollbar_tl.grid(row=0, column=1, pady=5, sticky=(tk.N, tk.S))
         self._text_log.configure(yscrollcommand=self._scrollbar_tl.set)
 
@@ -188,10 +233,17 @@ class RestTime:
 
     def _start_timer(self):
         if not self.timer_activo:
-            self.timer_contador = int(self._horas1_sv.get()) * 60 * 60 * 10 + int(self._horas2_sv.get()) * 60 * 60 +\
-                int(self._minutos1_sv.get()) * 60 * 10 + int(self._minutos2_sv.get()) * 60 + int(self._segundos1_sv.get()) * 10 + int(self._segundos2_sv.get())
+            self.timer_contador = int(self._horas1_sv.get()) * 60 * 60 * 10 +\
+                int(self._horas2_sv.get()) * 60 * 60 +\
+                int(self._minutos1_sv.get()) * 60 * 10 +\
+                int(self._minutos2_sv.get()) * 60 +\
+                int(self._segundos1_sv.get()) * 10 +\
+                int(self._segundos2_sv.get())
             if self.timer_contador > 0:
-                self.log(f"Iniciando timer {self._horas1_sv.get()}{self._horas2_sv.get()}:{self._minutos1_sv.get()}{self._minutos2_sv.get()}:{self._segundos1_sv.get()}{self._segundos2_sv.get()}...")
+                self.log(f"Iniciando timer {self._horas1_sv.get()}"
+                         f"{self._horas2_sv.get()}:{self._minutos1_sv.get()}"
+                         f"{self._minutos2_sv.get()}:{self._segundos1_sv.get()}"
+                         f"{self._segundos2_sv.get()}...")
                 self._start_btn.state(["disabled"])
                 self._horas1.state(["disabled"])
                 self._horas2.state(["disabled"])
@@ -203,7 +255,7 @@ class RestTime:
                 self.timer_activo = True
                 self.update_counter()
                 self._root.iconify()
-                # Necesita perder el foco en Windows para que funcione el aviso.
+                # Windows necesita perder el foco para que funcione el aviso.
                 if sys.platform == "win32":
                     hwnd_desktop = ctypes.windll.user32.GetDesktopWindow()
                     ctypes.windll.user32.SetForegroundWindow(hwnd_desktop)
@@ -226,11 +278,16 @@ class RestTime:
             self.timer_contador -= 1
             hor1 = int(self.timer_contador // 36000)
             hor2 = int((self.timer_contador - (hor1 * 36000)) // 3600)
-            min1 = int((self.timer_contador - (hor1 * 36000) - (hor2 * 3600)) // 600)
-            min2 = int((self.timer_contador - (hor1 * 36000) - (hor2 * 3600) - (min1 * 600)) // 60)
-            seg1 = int((self.timer_contador - (hor1 * 36000) - (hor2 * 3600) - (min1 * 600) - (min2 * 60)) // 10)
-            seg2 = int(self.timer_contador - (hor1 * 36000) - (hor2 * 3600) - (min1 * 600) - (min2 * 60) - (seg1 * 10))
-            self._root.title(f"{hor1}{hor2}:{min1}{min2}:{seg1}{seg2} {self.timer_contador}s")
+            min1 = int((self.timer_contador - (hor1 * 36000) -
+                        (hor2 * 3600)) // 600)
+            min2 = int((self.timer_contador - (hor1 * 36000) - (hor2 * 3600) -
+                        (min1 * 600)) // 60)
+            seg1 = int((self.timer_contador - (hor1 * 36000) - (hor2 * 3600) -
+                        (min1 * 600) - (min2 * 60)) // 10)
+            seg2 = int(self.timer_contador - (hor1 * 36000) - (hor2 * 3600) -
+                       (min1 * 600) - (min2 * 60) - (seg1 * 10))
+            self._root.title(f"{hor1}{hor2}:{min1}{min2}:{seg1}{seg2}"
+                             "{self.timer_contador}s")
             self._horas1_sv.set(hor1)
             self._horas2_sv.set(hor2)
             self._minutos1_sv.set(min1)
@@ -240,7 +297,7 @@ class RestTime:
 
             if self.timer_contador < 1:
                 self.log("*** FINALIZADO ***")
-                self.timer_activo=False
+                self.timer_activo = False
                 self._start_btn.state(["!disabled"])
                 self._start_btn.state(["!disabled"])
                 self._horas1.state(["!disabled"])
@@ -267,11 +324,11 @@ class RestTime:
         Hace parpadear la ventana en la barra de tareas
         """
         # FlashWindowEx function
-        #FLASHW_STOP = 0
-        #FLASHW_CAPTION = 1
+        # FLASHW_STOP = 0
+        # FLASHW_CAPTION = 1
         FLASHW_TRAY = 2
         FLASHW_ALL = 3
-        #FLASHW_TIMER = 4
+        # FLASHW_TIMER = 4
         FLASHW_TIMERNOFG = 12
 
         class FLASHWINFO(ctypes.Structure):
@@ -294,7 +351,8 @@ class RestTime:
 
     def muestra_parpadeo_linux(self):
         # Muestra el aviso en la ventana minimizada o sin foco.
-        atom_demands_attention = self.d.intern_atom('_NET_WM_STATE_DEMANDS_ATTENTION')
+        atom_demands_attention =\
+            self.d.intern_atom('_NET_WM_STATE_DEMANDS_ATTENTION')
         atom_wm_state = self.d.intern_atom('_NET_WM_STATE')
         event_data = event.ClientMessage(
             window=self.window_id,
@@ -302,13 +360,14 @@ class RestTime:
             data=(32, [1, atom_demands_attention, 0, 0, 0])
         )
         root = self.d.screen().root
-        root.send_event(event_data, event_mask=X.SubstructureNotifyMask | X.SubstructureRedirectMask)
+        root.send_event(event_data, event_mask=X.SubstructureNotifyMask |
+                        X.SubstructureRedirectMask)
         self.d.flush()
 
     def reproduce_sonidos(self):
-        current_path=os.path.dirname(os.path.abspath(__file__))
-        sonido1=os.path.join(current_path, 'Spo.wav')
-        sonido2=os.path.join(current_path, 'IsTimeToRest.amr')
+        current_path = os.path.dirname(os.path.abspath(__file__))
+        sonido1 = os.path.join(current_path, 'Spo.wav')
+        sonido2 = os.path.join(current_path, 'IsTimeToRest.amr')
         player = mpv.MPV()
         if os.path.exists(sonido1):
             for i in range(5):
@@ -372,6 +431,7 @@ class RestTime:
         pos_x = (ancho_pantalla - ancho_ventana) // 2
         pos_y = (alto_pantalla - alto_ventana) // 2
         self._root.geometry(f"+{pos_x}+{pos_y}")
+
 
 if __name__ == "__main__":
     app = RestTime()
